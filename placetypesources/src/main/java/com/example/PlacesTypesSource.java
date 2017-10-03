e73e4723589cbc7580c4c5272342d23f0cec0cfc
Deepa <malani.deepa87@gmail.com>
2017-10-03 10:10:17 -0700

    Add new place types.

--- placetypesources/src/main/java/com/example/PlacesTypesSource.java
+++ placetypesources/src/main/java/com/example/PlacesTypesSource.java
@@ -3,18 +3,19 @@ package com.example;
 public class PlacesTypesSource {
 
     private String[] placeTitles ={
-            "Atm", "Bakery", "Bank", "Bar",
-            "Bus station", "Cafe", "Church", "Gas station",
-            "Gym","Library","Mall","Theater","Park","Pharmacy",
-            "Restaurant","School"
+            "Airport","Atm", "Bakery", "Bank", "Bar","Beauty Salon",
+            "Book store","Bus station", "Cafe", "Church",
+            "Dentist","Doctor","Gas station", "Gym","Hospital",
+            "Library","Lodging", "Mall","Theater","Park","Pharmacy",
+            "Restaurant","School","Store","Train station","Taxi stand"
 
     };
 
     private String[] placeValues = {
-            "atm", "bakery","bank","bar",
-            "bus_station","cafe","church","gas_station",
-            "gym","library","shopping_mall","movie_theater",
-            "park","pharmacy","restaurant","school"
+             "airport","atm","bakery","bank","bar","beauty_salon",
+            "book_store","bus_station","cafe","church","dentist","doctor",
+            "gas_station", "gym","hospital","library","lodging","shopping_mall","movie_theater",
+            "park","pharmacy","restaurant","school","store","train_station","taxi_stand"
     };
 
     public String[] getPlaceTitles()
